0cfa61171641c53ab3b3ae419a8bcb229bcf7b4a
omenyrobert <macrobert000@gmail.com>
2022-12-02 20:07:10 +0300

    new

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,44 +1,35 @@
 <template>
     <div class="fixed-top">
-<div class="superNav border-bottom py-2 bg-light">
+<div class="superNav border-bottom py-2 bg-white">
   <div class="container">
     <div class="row">
-      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 centerOnMobile">
-        <select  class="me-3 border-0 bg-light">
-          <option value="en-us">EN-US</option>
-        </select>
+      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 centerOnMobile pt-1">
+      
         <span class="d-none d-lg-inline-block d-md-inline-block d-sm-inline-block d-xs-none me-3"><strong>info@somedomain.com</strong></span>
-        <span class="me-3"><i class="fa-solid fa-phone me-1 text-warning"></i> <strong>1-800-123-1234</strong></span>
-      </div>
+        <span class="me-3"><i class="fa-solid fa-phone me-1 text-warning"></i> 1-800-123-1234</span>
+        <span class="me-3"><i class="fa-solid fa-phone me-1 text-warning"></i>+256 778-1234</span>      
+    </div>
       <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 d-none d-lg-block d-md-block-d-sm-block d-xs-none text-end">
-        <span class="me-3"><i class="fa-solid fa-truck text-muted me-1"></i><a class="text-muted" href="#">Shipping</a></span>
-        <span class="me-3"><i class="fa-solid fa-file  text-muted me-2"></i><a class="text-muted" href="#">Policy</a></span>
+        <span class="me-3"><i class="bi bi-facebook text-primary" style="font-size: 18px;"></i></span>
+        <span class="me-3"><i class="bi bi-instagram text-primary"  style="font-size: 18px;"></i></span>
+        <span class="me-3"><i class="bi bi-telegram text-primary"  style="font-size: 18px;"></i></span>
+        <span class="me-3"><i class="bi bi-twitter text-primary"  style="font-size: 18px;"></i></span>
       </div>
     </div>
   </div>
 </div>
 <nav class="navbar navbar-expand-lg bg-white  navbar-light p-3 shadow-sm">
   <div class="container">
-    <a class="navbar-brand" href="#"><i class="fa-solid fa-shop me-2"></i> <strong>GEAR SHOP</strong></a>
+    <a class="navbar-brand" href="#"><i class="fa-solid fa-shop me-2"></i> <h5>Bethany Home and Guest House</h></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
  
     <div class="mx-auto my-3 d-lg-none d-sm-block d-xs-block">
-      <div class="input-group">
-        <span class="border-warning input-group-text bg-warning text-white"><i class="fa-solid fa-magnifying-glass"></i></span>
-        <input type="text" class="form-control border-warning" style="color:#7a7a7a">
-        <button class="btn btn-warning text-white">Search</button>
-      </div>
+      
     </div>
     <div class=" collapse navbar-collapse" id="navbarNavDropdown">
-      <div class="ms-auto d-none d-lg-block">
-        <div class="input-group">
-          <span class="border-warning input-group-text bg-warning text-white"><i class="fa-solid fa-magnifying-glass"></i></span>
-          <input type="text" class="form-control border-warning" style="color:#7a7a7a">
-          <button class="btn btn-warning text-white">Search</button>
-        </div>
-      </div>
+     
       <ul class="navbar-nav ms-auto ">
         <li class="nav-item">
           <a class="nav-link mx-2 text-uppercase active" aria-current="page" href="#">Offers</a>
